7c3b43d7551db8d835823f1a9f938e0ca06467cd
Andre Farias] <farias96andre@gmail.com>
2024-05-04 14:01:48 -0300

    feat: utilizando novas barras de menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 import GoogleMap from "./components/GoogleMap";
+import { CustomAppBar } from "./components/appBar/CustomAppBar"
+import './App.css';
+import {MenuLateral} from "./components/menuLateral/MenuLateral";
 
 function App() {
-  return (
-      <GoogleMap />
-  );
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className="App">
+            <CustomAppBar open={open} handleDrawerOpen={handleDrawerOpen} />
+            <MenuLateral open={open} handleDrawerClose={handleDrawerClose} />
+            <GoogleMap />
+        </div>
+    );
 }
+
 export default App;
