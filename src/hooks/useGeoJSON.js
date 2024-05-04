c97e2477de9070ab083c1e4ac12265f27678d8d4
Andre Farias] <farias96andre@gmail.com>
2024-05-04 01:34:52 -0300

    feat: criando hook personalizado para geojson

--- src/hooks/useGeoJSON.js
+++ src/hooks/useGeoJSON.js
@@ -1,15 +1,27 @@
-import { useRef, useState } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import { get } from '../service/terPazMapService';
-import usePolylineDrawer from './usePolylineDrawer'
+
 
 export function useGeoJSON(map) {
   const polygons = useRef([]);
-  const [streetsData, setStreetsData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [idNameList, setIdNameList] = useState([]);
 
-  // Use o hook usePolylineDrawer aqui
-  usePolylineDrawer(map, streetsData);
+  // Função para centralizar o mapa
+  const centerMap = (coordinates) => {
+    map.setCenter({ lat: coordinates[1], lng: coordinates[0] });
+    map.setZoom(15);
+  };
 
-  const fetchGeoJSON = async () => {
+  // Função para lidar com o clique no polígono
+  const handlePolygonClick = (mapData) => {
+    const center = mapData.properties.Centro.coordinates;
+    centerMap(center);
+  };
+
+  const fetchGeoJSON = useCallback(async () => {
+    setIsLoading(true);
+    let tempList = [];
     try {
       polygons.current.forEach(polygon => {
         polygon.setMap(null);
@@ -17,37 +29,38 @@ export function useGeoJSON(map) {
 
       polygons.current = [];
 
-      const userData = await get(); 
+      const userData = await get();
       if (map && userData) {
         const mapas = userData.features;
-        console.log(mapas);
-        mapas.forEach((mapData) => { 
+        mapas.forEach((mapData) => {
           if (mapData) {
-              const polygon = new window.google.maps.Polygon({
-                paths: mapData.geometry.coordinates[0].map(coord => ({ lat: coord[1], lng: coord[0] })),
-                map: map,
-                fillColor: '#87CEFA', 
-                fillOpacity: 0.35, 
-                strokeColor: '#1E90FF',
-                strokeOpacity: 0.8, 
-                strokeWeight: 2, 
-              });
+            const polygon = new window.google.maps.Polygon({
+              paths: mapData.geometry.coordinates[0].map(coord => ({ lat: coord[1], lng: coord[0] })),
+              map: map,
+              fillColor: '#87CEFA',
+              fillOpacity: 0.35,
+              strokeColor: '#1E90FF',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+            });
 
-              polygon.addListener('click', (event) => {
-                const center = mapData.properties.Centro.coordinates;
-                map.setCenter({ lat: center[1], lng: center[0] });
-                map.setZoom(15);
-              });
+            polygon.addListener('click', (event) => {
+              handlePolygonClick(mapData);
+            });
 
-              polygon.setMap(map);
-              polygons.current.push(polygon);
+            polygon.setMap(map);
+            polygons.current.push(polygon);
+
+            tempList.push({id: mapData.properties.ID, name: mapData.properties.Nome});
           }
         });
       }
     } catch (error) {
       console.error('Erro ao recuperar GeoJSON:', error);
     }
-  };
+    setIsLoading(false);
+    setIdNameList(tempList);
+  }, [map]);
 
-  return { fetchGeoJSON };
+  return { fetchGeoJSON, isLoading, idNameList, centerMap, handlePolygonClick };
 }
