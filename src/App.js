f52aea8c4c1137458ec00844d948149ee1b197d3
Andre Farias] <farias96andre@gmail.com>
2024-05-04 01:27:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import GoogleMap from "./components/GoogleMap";
 
 function App() {
-  return <GoogleMap />;
+  return (
+      <GoogleMap />
+  );
 }
-
 export default App;
--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
-import DrawingManager from "./DrawingManager";
+import DrawingManager from "./drawingManager/DrawingManager";
+import { useGeoJSON } from '../hooks/useGeoJSON';
+import RegionSelector from './RegionSelector'; // Importe o novo componente
+
 
 function GoogleMap() {
   const [map, setMap] = useState(null);
+  const [selectedRegion, setSelectedRegion] = useState(null);
+  const { fetchGeoJSON } = useGeoJSON(map, selectedRegion);
+
+  const regions = [
+    { id: 1, name: 'Região 1' },
+    { id: 2, name: 'Região 2' },
+  ];
 
   useEffect(() => {
     if (window.google && window.google.maps) {
@@ -15,9 +25,19 @@ function GoogleMap() {
     }
   }, []);
 
+  useEffect(() => {
+    if (selectedRegion) {
+      fetchGeoJSON();
+    }
+  }, [selectedRegion, fetchGeoJSON]);
+
   return (
-    <div id="map" style={{ width: '100%', height: '100vh' }}>
-      {map && <DrawingManager map={map} />}
+    <div style={{ position: 'relative' }}>
+      <RegionSelector regions={regions} onRegionChange={setSelectedRegion} />
+
+      <div id="map" style={{ width: '100%', height: '100vh' }}>
+        {map && <DrawingManager map={map} />}
+      </div>
     </div>
   );
 };
