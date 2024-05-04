fb3946b02ccd8bd0b1b76391edded8984888964f
Andre Farias] <farias96andre@gmail.com>
2024-05-04 01:33:59 -0300

    feat: criando hook personalizado para polylineDrawer

--- src/hooks/usePolylineDrawer.js
+++ src/hooks/usePolylineDrawer.js
@@ -4,15 +4,14 @@ function usePolylineDrawer(map, streetsData) {
   useEffect(() => {
     if (map && streetsData) {
       Object.entries(streetsData).forEach(([streetsType, streetsData]) => {
-        console.log(streetsData);
         if (streetsData.features) 
         streetsData.features.forEach(street => {
           const polyline = new window.google.maps.Polyline({
             path: street.geometry.coordinates.map(coord => ({ lat: coord[1], lng: coord[0] })),
             map: map,
-            strokeColor: streetsData.properties["stroke-color"], // Usa a cor da linha do objeto JSON
-            strokeOpacity: streetsData.properties['stroke-opacity'], // Usa a opacidade da linha do objeto JSON
-            strokeWeight: 2, // Define a espessura da linha
+            strokeColor: streetsData.properties["stroke-color"],
+            strokeOpacity: streetsData.properties['stroke-opacity'],
+            strokeWeight: 2, 
           });
 
           polyline.setMap(map);
