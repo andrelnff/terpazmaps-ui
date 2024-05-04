e0bc0e1d81c254a33ecdbfcbd117845678249929
Andre Farias] <farias96andre@gmail.com>
2024-05-04 14:00:10 -0300

    feat: removendo OverlayControls e adicionando logica para uso de controle personalizado dos desenhos

--- src/components/drawingManager/DrawingManager.js
+++ src/components/drawingManager/DrawingManager.js
@@ -1,23 +1,33 @@
 import React, { useEffect } from 'react';
 import { useDrawingManager } from '../../hooks/useDrawingManager';
 import { useGeoJSON } from '../../hooks/useGeoJSON';
-import OverlayControls from '../OverlayControls';
 import './DrawingManager.css';
 import Loading from '../loading/Loading';
+import DrawingControls from "../drawingControls/DrawingControls";
 
 function DrawingManager({ map }) {
-  const { deleteAllOverlays, savePolygonGeoJSON } = useDrawingManager(map);
+  const { deleteAllOverlays, savePolygonGeoJSON, drawingManager } = useDrawingManager(map);
   const { fetchGeoJSON, isLoading } = useGeoJSON(map);
- 
+
+  const setDrawingMode = (mode) => {
+    if (drawingManager) {
+      drawingManager.setDrawingMode(window.google.maps.drawing.OverlayType[mode]);
+    }
+  };
+
   useEffect(() => {
     fetchGeoJSON();
   }, [map, fetchGeoJSON]);
 
-  return (
-    <Loading isLoading={isLoading}>
-      <OverlayControls onDelete={deleteAllOverlays} onSave={savePolygonGeoJSON} />
-    </Loading>
-  );
-};
+    return (
+        <Loading isLoading={isLoading}>
+            <DrawingControls
+                setDrawingMode={setDrawingMode}
+                savePolygonGeoJSON={savePolygonGeoJSON}
+                deleteAllOverlays={deleteAllOverlays}
+            />
+        </Loading>
+    );
+}
 
 export default DrawingManager;
