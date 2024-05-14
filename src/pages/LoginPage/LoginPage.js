c17630185ffefb4b9a499111af5487b26aa280b3
Andre Farias] <farias96andre@gmail.com>
2024-05-14 01:37:51 -0300

    refactor: adicionando melhorias visuais

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -3,6 +3,9 @@ import './LoginPage.css';
 import {useAuth} from "../../context/authContext";
 import {useNavigate} from "react-router-dom";
 import terpaz from "../../assets/terpaz.png";
+import {TextField} from "@mui/material";
+import '@fontsource/roboto';
+
 
 function LoginPage() {
     const navigate = useNavigate();
@@ -36,14 +39,26 @@ function LoginPage() {
                     <p>Digite os seus dados de acesso nos campos abaixo.</p>
                 </div>
                 <div className="input-group">
-                    <label className="login-label">Nome de usuário:</label>
-                    <input className="field-input" type="text" value={username}
-                           onChange={(e) => setUsername(e.target.value)}/>
+                    <TextField
+                        className="field-input"
+                        label="Nome de usuário"
+                        variant="outlined"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        fullWidth
+                    />
                 </div>
                 <div className="input-group">
-                    <label className="login-label">Senha:</label>
-                    <input className="field-input" type="password" value={password}
-                           onChange={(e) => setPassword(e.target.value)}/>
+                    <TextField
+                        className="field-input"
+                        label="Senha"
+                        type="password"
+                        variant="outlined"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        fullWidth
+                        margin="normal" // Adiciona um espaçamento
+                    />
                 </div>
                 {error && <span key={Date.now()} className="fade-out">Email ou senha incorretas!</span>}
                 <button className="botao-entrar" type="submit">Entrar</button>
@@ -51,10 +66,8 @@ function LoginPage() {
                     Ainda não possui cadastro? <a href="/cadastro">Clique aqui</a>
                 </div>
             </form>
-
         </div>
     );
 }
-
 
 export default LoginPage;
