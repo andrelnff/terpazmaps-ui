e7fe901e77e7f076b3d15e635cab50ac950478fa
Andre Farias] <farias96andre@gmail.com>
2024-05-13 23:35:46 -0300

    feat: criando pagina de login

---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react';
+import './LoginPage.css';
+import {useAuth} from "../../context/authContext";
+import {useNavigate} from "react-router-dom";
+
+function LoginPage() {
+    const navigate = useNavigate();
+
+    const [error, setError] = useState(false);
+    const {login} = useAuth();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    useEffect(() => setError(false), [username]);
+    useEffect(() => setError(false), [password]);
+
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            await login(username, password);
+            navigate('/maps');
+        } catch (error) {
+            console.error(error);
+            setError(true);
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <form className="login-form" onSubmit={handleLogin}>
+                <div className="label-form">
+                    <h1>Login</h1>
+                    <p>Digite os seus dados de acesso nos campos abaixo.</p>
+                </div>
+                <div className="input-group">
+                    <label className="login-label">Nome de usuário:</label>
+                    <input className="field-input" type="text" value={username}
+                           onChange={(e) => setUsername(e.target.value)}/>
+                </div>
+                <div className="input-group">
+                    <label className="login-label">Senha:</label>
+                    <input className="field-input" type="password" value={password}
+                           onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                {error && <span key={Date.now()} className="fade-out">Email ou senha incorretas!</span>}
+                <button className="botao-entrar" type="submit">Entrar</button>
+                <div className="box-sign">
+                    Ainda não possui cadastro? <a href="/cadastro">Clique aqui</a>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+
+export default LoginPage;
