4d68849877ee38c0aa28aee3adefdf7025f30593
Andre Farias] <farias96andre@gmail.com>
2024-05-18 15:44:48 -0300

    refactor: corrigindo comportamento de usePolylineDrawer

--- src/hooks/usePolylineDrawer.js
+++ src/hooks/usePolylineDrawer.js
@@ -1,24 +1,40 @@
-import { useEffect } from 'react';
+import { useRef, useState } from 'react';
+import {getStreetData} from "../service/terPazMapService";
 
-function usePolylineDrawer(map, streetsData) {
-  useEffect(() => {
-    if (map && streetsData) {
-      Object.entries(streetsData).forEach(([streetsType, streetsData]) => {
-        if (streetsData.features) 
-        streetsData.features.forEach(street => {
+export function usePolylineDrawer(map) {
+  const polylines = useRef([]);
+  const [streetsData, setStreetsData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading] = useState(false); // Estado de carregamento para as ruas
+
+  const drawStreets = async (regionId, setLoading) => {
+    setLoading(true);
+    polylines.current.forEach(polyline => polyline.setMap(null));
+    polylines.current = [];
+
+    try {
+      const data = await getStreetData(regionId);
+      setStreetsData(data);
+      if (data && data.features) {
+        data.features.forEach(street => {
           const polyline = new window.google.maps.Polyline({
             path: street.geometry.coordinates.map(coord => ({ lat: coord[1], lng: coord[0] })),
             map: map,
-            strokeColor: streetsData.properties["stroke-color"],
-            strokeOpacity: streetsData.properties['stroke-opacity'],
-            strokeWeight: 2, 
+            strokeColor: street.properties.color,
+            strokeOpacity: street.properties['stroke-opacity'],
+            strokeWeight: 2,
           });
 
           polyline.setMap(map);
+          polylines.current.push(polyline);
         });
-      });
+      }
+    } catch (err) {
+      setError(err.message);
+      console.error('Erro ao buscar dados das ruas:', err);
     }
-  }, [map, streetsData]);
-}
+    setLoading(false);
+  };
 
-export default usePolylineDrawer;
+  return { streetsData, error, isLoading, drawStreets };
+}
