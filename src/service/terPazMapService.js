a3dd1c0dc7342949e271f33f7e7917d0f3955a62
Andre Farias] <farias96andre@gmail.com>
2024-05-18 10:56:25 -0300

    feat: adicionando servico para obter informacoes sobre tipos de ruas

--- src/service/terPazMapService.js
+++ src/service/terPazMapService.js
@@ -1,18 +1,23 @@
-const API_URL = '/public/api/v5/geojson/region';
-
-export async function get() {
-    const response = await fetch(API_URL);
+export const getAllRegions = async () => {
+    const response = await fetch('/public/api/v5/geojson/region');
     if (!response.ok) {
-      throw new Error(`GET request failed with status ${response.status}`);
+        throw new Error(`GET request failed with status ${response.status}`);
     }
     return await response.json();
-}
+};
 
+export const getStreetData = async (regionId) => {
+    const response = await fetch(`/public/api/v5/geojson/region/${regionId}/streets`);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+};
 
-export async function getStreetData(regionId) {
-  const response = await fetch(`https://criteriontecnologia.store/public/api/v5/geojson/region/${regionId}/streets`);
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  return await response.json();
-}
+export const getStreetConditions = async () => {
+    const response = await fetch('/public/api/v5/geojson/street_condition');
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+};
