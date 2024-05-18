43753906491493340a2e693777816159575b05f1
Andre Farias] <farias96andre@gmail.com>
2024-05-18 13:28:55 -0300

    feat: adicionando mapProvider na aplicação

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { AuthProvider } from "./context/authContext";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import MapsPage from "./pages/MapsPage/MapsPage";
+import {MapProvider} from "./context/mapContext";
 
 function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/maps" element={<MapsPage />} />
-                    <Route path="*" element={<Navigate to="/login" replace />} />
-                </Routes>
+                <MapProvider>
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/maps" element={<MapsPage />} />
+                        <Route path="*" element={<Navigate to="/login" replace />} />
+                    </Routes>
+                </MapProvider>
             </AuthProvider>
         </BrowserRouter>
     );
