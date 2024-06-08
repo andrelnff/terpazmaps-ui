37aed5b6930db80f5375e46dd58d87c74b4759c4
Andre Farias] <farias96andre@gmail.com>
2024-06-08 09:23:18 -0300

    feat: adicionando componentes a pagina de mapas

--- src/pages/MapsPage/MapsPage.js
+++ src/pages/MapsPage/MapsPage.js
@@ -4,10 +4,14 @@ import {MenuLateral} from "../../components/menuLateral/MenuLateral";
 import GoogleMap from "../../components/googleMap/GoogleMap";
 import {isTokenValid} from "../../service/authService";
 import {useNavigate} from "react-router-dom";
+import {useMapCenter} from "../../hooks/useMapCenter";
+import StarRating from "../../components/rating/Rating";
+import MenuInferior from "../../components/menuInferior/MenuInferior";
 
 function MapsPage() {
     const navigate = useNavigate();
     const [open, setOpen] = useState(false);
+    const { selectedMapId } = useMapCenter();
 
     const handleDrawerOpen = () => {
         setOpen(true);
@@ -33,6 +37,8 @@ function MapsPage() {
             <CustomAppBar open={open} handleDrawerOpen={handleDrawerOpen} />
             <MenuLateral open={open} handleDrawerClose={handleDrawerClose} />
             <GoogleMap />
+            <StarRating mapId={selectedMapId}></StarRating>
+            <MenuInferior></MenuInferior>
         </div>
     );
 }
